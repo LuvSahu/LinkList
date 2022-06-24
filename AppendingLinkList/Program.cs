85932e1fd2cfb366e99ee1a818b072f440a58910
Luv sahu <lksahu404@gmail.com>
2022-06-24 11:03:34 +0530

    This is code for Appending LinkList

---
+++ AppendingLinkList/Program.cs
@@ -0,0 +1,28 @@
+﻿using LinkListAss;
+
+namespace LinkedListAss
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            LinkList Link = new LinkList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(56);
+            Node node3 = new Node(30);
+            Node node4 = new Node(40);
+
+
+            Link.AddNode(node1);
+            Link.AddNode(node2);
+            Link.AddNode(node3);
+            Link.AppendNode(node4);
+
+            Link.Display();
+
+
+
+        }
+    }
+}
