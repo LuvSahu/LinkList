fa83ff9a50f16656388d98ae0f5219d84bbddbc8
Luv sahu <lksahu404@gmail.com>
2022-06-24 12:13:19 +0530

    This code for Search a node in LinkList

---
+++ SearchLinkListFindNode/Program.cs
@@ -0,0 +1,41 @@
+﻿using LinkListAss;
+
+namespace LinkedListAss
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            LinkList Link = new LinkList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(56);
+            Node node3 = new Node(30);
+            Node node4 = new Node(40);
+            Node node5 = new Node(95);
+
+
+            Link.AddNode(node1);
+            Link.AddNode(node2);
+            Link.AddNode(node3);
+            Link.AppendNode(node4);
+
+            Link.InsertNode(node5, node2);
+            Link.Display();
+
+            Link.DeleteNodeAtFirst();
+            Link.Display();
+
+            Link.DeleteNodeAtLast();
+            Console.WriteLine("\n Delete at node at last");
+            Link.Display();
+
+            Console.WriteLine("\n");
+            Console.WriteLine( Link.Search(2));
+
+
+
+
+        }
+    }
+}
