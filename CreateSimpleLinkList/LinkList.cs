5edc644bdca8606e8248aa7f1b85e031278e239a
Luv sahu <lksahu404@gmail.com>
2022-06-24 10:10:27 +0530

    This is code for Create a Simple LinkList

---
+++ CreateSimpleLinkList/LinkList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkListAss
+{
+    public class LinkList
+    {
+      
+            public Node Head;
+            public Node Tail;
+            public LinkList()
+            {
+                Head = null;
+                Tail = null;
+            }
+
+            public void AddNode(Node node)
+            {
+                if (Head == null && Tail == null)
+                {
+                    Head = node;
+                    Tail = node;
+                }
+                else
+                {
+                    node.next = Head;
+                    Head = node;
+                }
+            }
+        }
+    }
+
---
+++ CreateSimpleLinkList/Program.cs
@@ -0,0 +1,22 @@
+﻿using LinkListAss;
+
+namespace LinkedListAss
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            LinkList Link = new LinkList();
+            Node node1 = new Node(70);
+            Node node2 = new Node(56);
+            Node node3 = new Node(30);
+
+            Link.AddNode(node1);
+            Link.AddNode(node2);
+            Link.AddNode(node3);
+
+
+        }
+    }
+}
